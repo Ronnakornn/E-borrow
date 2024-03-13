d2a305c4f20cc57124a0f6e0573a04a72e5fd625
Ronnakornn <Ronnakon.sc@gmail.com>
2024-03-13 12:34:46 +0700

    Change logo

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center gap-x-2">
-    <img src="{{ asset('/images/logo.jpg') }}" alt="Logo" class="h-16">
+    <img src="{{ asset('/images/logo.png') }}" alt="Logo" class="h-16">
     {{-- <span class="self-center leading-5 tracking-tight text-xl font-bold whitespace-nowrap dark:text-white">E-ฺBorrow</span> --}}
 </div>
